921d03683cda85d7984d1347c211fdde30ed2678
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-15 19:16:30 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, ingredients_attributes: [:ingredient_1, :ingredient_2])
+    params.require(:recipe).permit(:name, ingredients_attributes: [:id, :ingredient_1, :ingredient_2])
   end
 end
